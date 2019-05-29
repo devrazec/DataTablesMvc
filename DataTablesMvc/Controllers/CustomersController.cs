8f7caed11eb50a8c081d53bdf7197a8938e9c76a
devrazec@outlook.com <devrazec@outlook.com>
2019-05-28 23:50:18 -0300

    DataTables Buttons

--- DataTablesMvc/Controllers/CustomersController.cs
+++ DataTablesMvc/Controllers/CustomersController.cs
@@ -1,8 +1,6 @@
 ﻿using DataTablesMvc.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace DataTablesMvc.Controllers
@@ -23,6 +21,17 @@ namespace DataTablesMvc.Controllers
             List<Customers> lstCustomers = db.Customers.ToList<Customers>();
 
             return Json(new { data = lstCustomers }, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public ActionResult AddOrEdit(int id = 0)
+        {
+            if (id == 0)
+                return View(new Customers());
+            else
+            {
+                return View(db.Customers.Where(x => x.CustomerID == id).FirstOrDefault<Customers>());
+            }
         }
     }
 }
